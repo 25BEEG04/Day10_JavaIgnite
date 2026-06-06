92ff6349cb340c33a6a25e83d640b8101159a42a
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-06 14:55:38 +0530

    Update ATM_check.java

--- ATM_check.java
+++ ATM_check.java
@@ -1,13 +1,25 @@
-/*
-ATM Multi-Level Exception System
+package JavaIgniteDay10;
+import java.util.Scanner;
+public class ATM_check {
 
-Create a program that simulates ATM withdrawal.
-
-Requirements:
-Take balance and withdrawal amount
-If withdrawal > balance → handle exception
-If withdrawal amount is negative → handle exception
-If input is invalid (text instead of number) → handle exception
-Expected Output:
-Transaction Successful / Insufficient Balance / Invalid Input
-*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int b,w,r;
+		try {
+			System.out.println("Enter the account balance :");
+			b=sc.nextInt();
+			System.out.println("Enter the withdrawl amount :");
+			w=sc.nextInt();
+			if(w > b )
+				System.out.println("Invalid input");
+			if(w < 0 )
+				System.out.println("Withdrawl amount cannot be negative");
+			r = b-w;
+			System.out.println("Withdraw Sucessfully \n Remaining Balance is: "+ r) ;
+		}
+		catch(Exception e) {
+			System.out.println("Invalid input\n Please enter a valid number");
+		}
+		sc.close();
+	}
