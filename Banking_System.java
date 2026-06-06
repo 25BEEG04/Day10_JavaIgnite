9adf4b2d801e5024f240a7c72cd63ce3b47f07a6
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-06 17:18:34 +0530

    Update Banking_System.java

--- Banking_System.java
+++ Banking_System.java
@@ -1,13 +1,31 @@
-/*
-Banking System (Real-world Simulation)
+package JavaIgniteDay10;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Banking_system {
 
-Create a program that:
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int b,w;
+		try {
+			System.out.println("Enter the balance :");
+			b=sc.nextInt();
+			System.out.println("Enter the withdrawl amount :");
+			w=sc.nextInt();
+			
+			if(w > b ) {
+				throw new Exception("In sufficent balance");
+			}
+			System.out.println("Withdrawl sucess !");
+			System.out.println("Remaining balance :"+ (b-w));
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Invalid input");
+		}
+		catch(Exception e) {
+			System.out.println("In sufficent balance");
+		}
+		sc.close();
+	}
+}
 
-Takes account balance
-Takes withdrawal amount
-Rules:
-If withdrawal > balance → throw and handle exception
-If invalid input → handle exception
-Output:
-Withdrawal successful OR Insufficient balance
-*/
