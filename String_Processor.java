7ea330328e23aafc91e47051cc39b1a5fad978dd
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-06 17:21:47 +0530

    Update String_Processor.java

--- String_Processor.java
+++ String_Processor.java
@@ -1,19 +1,27 @@
-/*
-String Processor with Exception Safety
+package JavaIgniteDay10;
+import java.util.Scanner;
+public class String_Processor {
 
-Create a program that:
-
-Takes a string input
-
-Performs:
-
-substring operation
-charAt operation
-Requirements:
-
-Handle:
-
-StringIndexOutOfBoundsException
-Example:
-Invalid index handled safely
-*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str;
+		System.out.println("Enter the string");
+		s = sc.nextLine();
+		System.out.println("Main is "+ s);
+		
+		try {
+			System.out.println("Enter the index for substring(beginning index / end index) ");
+			int beg = sc.nextInt();
+			int end = sc.nextInt();
+			System.out.println(str.substring(beg,end));
+			System.out.println("Enter the index you want to view :");
+			int index = sc.nextInt();
+			System.out.println(str.charAt(index));
+		}
+		catch(StringIndexOutOfBoundsException e){
+			System.out.println("Invalid index");
+		}
+		sc.close();
+	}
+}
