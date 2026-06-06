12618c3e52939a41ad7ade7a0c343fdcf5c4b5e1
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-06 17:31:30 +0530

    Update Student_Marks.java

--- Student_Marks.java
+++ Student_Marks.java
@@ -1,12 +1,32 @@
-/*
-Student Marks System (Safe Input)
+package JavaIgniteDay10;
+import java.util.Scanner;
+public class Student_mark {
 
-Create a program that:
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num,i,index;
+		System.out.println("Enter the number of students :");
+		num=sc.nextInt();
+		int []marks= new int [num];
+		System.out.println("Enter the marks:");
+		for(i=0;i<num;i++) {
+			marks[i]=sc.nextInt();
+		}
+		
+		try {
+			System.out.println("Enter the index");
+			index = sc.nextInt();
+			
+			 System.out.println("Marks = " + marks[index]);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid index !!");
+		}
+		catch(Exception err) {
+			System.out.println("Invalid Input");
+		}
+		sc.close();
+	}
 
-Stores 5 student marks in an array
-Takes index input from user
-Displays mark
-Add Exception Handling:
-Handle invalid index
-Handle invalid input (if user enters text instead of number)
-*/
+}
